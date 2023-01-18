007163828378e35c354d85c45a7df3d2885ba668
Valen <valenpschool@gmail.com>
2023-01-18 20:30:11 -0300

    feat: add new components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,19 @@
 import './styles/global.css'
-import { Habit } from "./components/habit"
+import { Header } from './components/header'
+import { SummaryTable } from './components/summaryTable'
+//import { Habit } from "./components/habit"
 
-function App() {
+export function App() {
 
 
   return (
-   <div>
-      <Habit completed={3} />
-      <Habit completed={5} />
-      <Habit completed={10} />
+   <div className='w-screen h-screen flex justify-center items-center'>
+    <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+    <Header />
+     <SummaryTable/>
+   </div>
    </div>
   )
 }
 
-export default App
+
